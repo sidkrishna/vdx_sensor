148aff09ccea666ee4be43e1b9ecfb9cd51d17c5
Darin Sikanic <dsikanic@brocade.com>
2016-08-09 15:24:38 +1000

    Calculating the running average of metrics and using it for the threshold comparison

--- sensors/vdx_utilisation_sensor.py
+++ sensors/vdx_utilisation_sensor.py
@@ -21,13 +21,36 @@ class VDXUtilisationSensor(VDXBaseSensor):
     def poll(self):
         self._logger.info("Utilisation Sensor Polling")
         interfaces = super(VDXUtilisationSensor, self).poll()
-        self._check_threshold(interfaces)
+        average_metrics = self._calc_average_metrics(interfaces)
+        self._set_metrics(average_metrics)
+        self._check_threshold(average_metrics)
 
-    def _check_threshold(self, interfaces):
+    def _calc_average_metrics(self, interfaces):
+        prev_metrics = self._get_metrics()
+        if prev_metrics is None:
+            return interfaces
+        avg_metrics = {}
         for interface_name, interface_stats in interfaces.iteritems():
+            avg_metrics[interface_name] = {}
+            for metric, metric_value in prev_metrics[interface_name].iteritems():
+                avg_metrics[interface_name][metric] = (int(interface_stats[metric]) + int(metric_value)) / 2
+        return avg_metrics
+
+    def _set_metrics(self, metrics):
+        if hasattr(self._sensor_service, 'set_value'):
+            self._sensor_service.set_value(name='metrics', value=metrics)
+
+    def _get_metrics(self):
+        if hasattr(self._sensor_service, 'get_value'):
+            metrics = self._sensor_service.get_value(name='metrics')
+            if metrics:
+                return ast.literal_eval(metrics)
+
+    def _check_threshold(self, metrics):
+        for interface_name, interface_stats in metrics.iteritems():
             for metric, metric_value in self._config['sensor_utilisation']['metric_thresholds'].iteritems():
                 if int(interface_stats[metric]) > int(metric_value):
-                    self._dispatch_trigger(interface_name, interfaces[interface_name], metric, metric_value)
+                    self._dispatch_trigger(interface_name, metrics[interface_name], metric, metric_value)
 
     def _dispatch_trigger(self, interface_name, interface_stats, metric, metric_value):
         trigger = self._trigger_ref
